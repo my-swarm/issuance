6e211586f74c78bc268ed0f312bf7ef16f4ba28a
Jiri Blaha <blaha@wedecom.cz>
2020-12-22 11:31:43 +0100

    Better graphql reset

--- @app/useGraphql.ts
+++ @app/useGraphql.ts
@@ -7,12 +7,19 @@ interface UseGraphqlResult {
 export function useGraphql() {
   const client = useApolloClient();
 
-  const timeouts = [1000, 3000, 9000];
-  const reset = () => {
+  const timeouts = [3000, 9000];
+
+  function doReset() {
+    client.stop();
+    client.cache.reset();
     client.resetStore();
+  }
+
+  const reset = () => {
+    doReset();
     for (const timeout of timeouts) {
       window.setTimeout(() => {
-        client.resetStore();
+        doReset();
       }, timeout);
     }
   };

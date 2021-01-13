894acb1db8b78eee5c8358965392bc390c6661b2
Jiri Blaha <blaha@wedecom.cz>
2021-01-13 11:14:43 +0100

    Simplified main menu (no investor/issuer division)

--- @components/layout/MainMenu.tsx
+++ @components/layout/MainMenu.tsx
@@ -22,30 +22,21 @@ export function MainMenu(): ReactElement {
           <a>Dashboard</a>
         </Link>
       </Menu.Item>{' '}
-      <Menu.SubMenu key="investor" title={<span>Investor</span>} icon={<UserOutlined />} popupOffset={[16, 0]}>
-        <Menu.Item key="/invest" icon={<DollarCircleOutlined />}>
-          <Link href="/invest">
-            <a>Contribute</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/wallet" icon={<WalletOutlined />}>
-          <Link href="/wallet">
-            <a>Wallet</a>
-          </Link>
-        </Menu.Item>
-      </Menu.SubMenu>
-      <Menu.SubMenu key="issuer" title="Token Issuer" icon={<UserOutlined />}>
-        <Menu.Item key="/tokens" icon={<AppstoreOutlined />}>
-          <Link href="/tokens">
-            <a>Tokens</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/fundraisers" icon={<LineChartOutlined />}>
-          <Link href="/fundraisers">
-            <a>Fundraisers</a>
-          </Link>
-        </Menu.Item>
-      </Menu.SubMenu>
+      <Menu.Item key="/invest" icon={<DollarCircleOutlined />}>
+        <Link href="/invest">
+          <a>Contribute</a>
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="/wallet" icon={<WalletOutlined />}>
+        <Link href="/wallet">
+          <a>Wallet</a>
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="/tokens" icon={<AppstoreOutlined />}>
+        <Link href="/tokens">
+          <a>Issue Tokens</a>
+        </Link>
+      </Menu.Item>
       <Menu.Item key="about" icon={<InfoCircleOutlined />}>
         <a href="https://www.swarmnetwork.org/" target="_blank" rel="noreferrer noopener">
           About

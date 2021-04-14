d8f0d3b14da72b3fe5ad02e56dcb4c7b490fa0d1
Jiri Blaha <blaha@wedecom.cz>
2021-04-14 17:29:03 +0200

    Fundraisers page cleanup

--- pages/fundraisers.tsx
+++ pages/fundraisers.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import Link from 'next/link';
-import { Button, Space } from 'antd';
 
 import { DefaultLayout, FundraiserIssuerCard, Loading } from '@components';
-import { useAppState, useEthers } from '@app';
+import { useEthers } from '@app';
 import { useFundraisersQuery } from '@graphql';
 
 export default function Fundraisers() {
-  const [{ tokens }, dispatch] = useAppState();
   const { address } = useEthers();
-  const { data, loading, error } = useFundraisersQuery({ variables: { owner: address } });
+  const { data, loading } = useFundraisersQuery({ variables: { owner: address } });
   if (loading) return <Loading />;
   const fundraisers = data?.fundraisers || [];
 
-  const renderHeadExtra = () => (
-    <Space>
-      <Button type="primary">Start new Fundraiser</Button>
-      <Link href="/tokens">
-        <Button>Manage my Tokens</Button>
-      </Link>
-    </Space>
-  );
   return (
     <DefaultLayout title="My fundraisers">
       {fundraisers.length > 0 ? (

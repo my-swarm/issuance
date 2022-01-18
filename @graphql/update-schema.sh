cd41c6d3e6f76f5ab1514e926077bf61136831ad
Jiri Blaha <blaha@wedecom.cz>
2022-01-18 16:11:01 +0100

    codewatch graphql schema with local subgraph only

--- @graphql/update-schema.sh
+++ @graphql/update-schema.sh
@@ -3,4 +3,7 @@
 ENDPOINT_URL=$(grep -oP '^NEXT_PUBLIC_GRAPHQL_ENDPOINT=\K.*' ../.env.local)
 echo "Updating from $ENDPOINT_URL"
 
-npx get-graphql-schema $ENDPOINT_URL > schema.graphql
+if [ ENDPOINT_URL == "http://localhost:8100/subgraphs/name/my-swarm/issuance" ]
+then
+  npx get-graphql-schema $ENDPOINT_URL > schema.graphql
+fi
